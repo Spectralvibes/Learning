c767af88cc564c3aeaae5080b278b3145de50b3a
Spectralvibes <spectralvibes@gmail.com>
2022-05-02 20:59:35 +0530

    Initial commit

---
+++ C C++/Introduction/HelloWorld.c
@@ -0,0 +1,14 @@
+/**
+ * First C programme
+ *
+ * @author  Sandip Nimbhore
+ * @version 1.0, 02.05.2022
+ * @since   Release 1.0
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to the Sea of C!\n");
+    return 0;
+}
