2bf9dd4c5bfb89eca5bc3fd45e84e17f81515f1c
Spectralvibes <spectralvibes@gmail.com>
2022-08-18 19:28:58 +0530

    Changes

---
+++ C C++/Codebase/1. Getting started/HelloWorld.c
@@ -0,0 +1,13 @@
+/**
+* First C program
+* Prints Hello world!
+*
+* @author Sandip Nimbhore
+* @version 1.0, 02.05.2022
+* @since Release 1.0
+*/
+#include <stdio.h>
+int main() {
+	printf("Hello world!\n");
+	return 0;
+}
---
+++ C C++/Codebase/1. Getting started/sum.c
@@ -0,0 +1,14 @@
+/**
+* Adds two numbers
+*
+* @author Sandip Nimbhore
+* @version 1.0, 02.05.2022
+* @since Release 1.0
+*/
+#include <stdio.h>
+int main() {
+	int firstNumber = 10;
+	int secondNumber = 20;
+	printf("Sum: %d \n", firstNumber + secondNumber);
+	return 0;
+}
